02e704ed2ba6626e441c0a841cd7febd7cd090b5
Maxwell Croy <max.croy1@gmail.com>
2020-05-27 16:24:27 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,12 @@
+# Don't do this:
+puts ( 1 + 1)
+
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+name = "Byron"
+# name = "Luca"
+
+puts "We're sorry, but per health inspector's rules, #{name} is not allowed in the store."
